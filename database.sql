a2486ee1453bb979e4d587c5e8a8a8bd6f462b1d
Sebastian-Velazquez <sergiovelazquez1693@gmail.com>
2023-10-26 11:31:59 -0300

    trabajo de inmobiliaria

---
+++ database.sql
@@ -0,0 +1,2 @@
+CREATE DATABASE IF NOT EXISTS inmobiliaria;
+USE inmobiliaria;
---
+++ resources/views/layouts/header-footer.blade.php
@@ -0,0 +1,211 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+<title>Inmobiliaria Flores</title>
+<meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+ 	<link rel="stylesheet" href="assets/bootstrap/css/bootstrap.css" />
+  <link rel="stylesheet" href="assets/style.css"/>
+  <script src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
+	<script src="assets/bootstrap/js/bootstrap.js"></script>
+  <script src="assets/script.js"></script>
+
+    <!-- Scripts -->
+    {{-- @vite(['resources/sass/app.scss', 'resources/js/app.js']) --}}
+
+<!-- Owl stylesheet -->
+<link rel="stylesheet" href="assets/owl-carousel/owl.carousel.css">
+<link rel="stylesheet" href="assets/owl-carousel/owl.theme.css">
+<script src="assets/owl-carousel/owl.carousel.js"></script>
+<!-- Owl stylesheet -->
+
+
+<!-- slitslider -->
+    <link rel="stylesheet" type="text/css" href="assets/slitslider/css/style.css" />
+    <link rel="stylesheet" type="text/css" href="assets/slitslider/css/custom.css" />
+    <script type="text/javascript" src="assets/slitslider/js/modernizr.custom.79639.js"></script>
+    <script type="text/javascript" src="assets/slitslider/js/jquery.ba-cond.min.js"></script>
+    <script type="text/javascript" src="assets/slitslider/js/jquery.slitslider.js"></script>
+<!-- slitslider -->
+
+</head>
+
+<body>
+  @if (isset(Auth::user()->name))
+      
+  {{ Auth::user()->name }} estoy logueado
+  @endif
+
+<!-- Header Starts -->
+<div class="navbar-wrapper">
+  
+        <div class="navbar-inverse" role="navigation">
+          <div class="container">
+            <div class="navbar-header">
+
+
+              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+              </button>
+
+            </div>
+
+
+            <!-- Nav Starts -->
+            <div class="navbar-collapse  collapse">
+              <ul class="nav navbar-nav navbar-right">
+               <li class="active"><a href="index.php">Inicio</a></li>
+                <li><a href="about.php">Acerca de nosotros</a></li>
+                <li><a href="agents.php">Agentes</a></li>         
+                <li><a href="blog.php">Blog</a></li>
+                <li><a href="contact.php">Contacto</a></li>
+              </ul>
+            </div>
+            <!-- #Nav Ends -->
+
+          </div>
+        </div>
+
+    </div>
+<!-- #Header Starts -->
+
+
+
+
+
+<div class="container">
+
+<!-- Header Starts -->
+<div class="header">
+<a href="index.php"><img src="images/logo.png" alt="Realestate"></a>
+
+              <ul class="pull-right">
+                <li><a href="buysalerent.php">Comprar</a></li>
+                <li><a href="buysalerent.php">Vender</a></li>         
+                <li><a href="buysalerent.php">Alquilar</a></li>
+              </ul>
+</div>
+<!-- #Header Starts -->
+</div>
+
+
+<main class="py-4">
+  @yield('content')
+</main>
+
+
+
+
+
+<div class="footer">
+
+  <div class="container">
+  <div class="row">
+              <div class="col-lg-3 col-sm-3">
+                     <h4>Información</h4>
+                     <ul class="row">
+                  <li class="col-lg-12 col-sm-12 col-xs-3"><a href="about.php">Acerca de nosotros</a></li>
+                  <li class="col-lg-12 col-sm-12 col-xs-3"><a href="agents.php">Agentes</a></li>         
+                  <li class="col-lg-12 col-sm-12 col-xs-3"><a href="blog.php">Blog</a></li>
+                  <li class="col-lg-12 col-sm-12 col-xs-3"><a href="contact.php">Contacto</a></li>
+                </ul>
+              </div>
+              
+              <div class="col-lg-3 col-sm-3">
+                      <h4>Notificaciones</h4>
+                      <p>Reciba notificaciones sobre las últimas propiedades en nuestro mercado.</p>
+                      <form class="form-inline" role="form">
+                              <input type="text" placeholder="Ingrese su email" class="form-control">
+                                  <button class="btn btn-success" type="button">¡Notificarme!</button></form>
+              </div>
+              
+              <div class="col-lg-3 col-sm-3">
+                      <h4>Seguinos en nuestras redes</h4>
+                      <a href="#"><img src="images/facebook.png" alt="facebook"></a>
+                      <a href="#"><img src="images/twitter.png" alt="twitter"></a>
+                      <a href="#"><img src="images/linkedin.png" alt="linkedin"></a>
+                      <a href="#"><img src="images/instagram.png" alt="instagram"></a>
+              </div>
+  
+               <div class="col-lg-3 col-sm-3">
+                      <h4>Contacto</h4>
+                      <p><b>Inmobiliaria Flores</b><br>
+  <span class="glyphicon glyphicon-map-marker"></span> Julio a Roca 000, Villa Gobernador Galvez <br>
+  <span class="glyphicon glyphicon-envelope"></span> inmobiliariafloresvgg@gmail.com<br>
+  <span class="glyphicon glyphicon-earphone"></span> (341) 999-9999</p>
+              </div>
+          </div>
+  <p class="copyright">Copyright 2023. Todos los derechos reservados.	</p>
+  
+  
+  </div></div>
+  
+  
+  
+  
+  <!-- Modal -->
+  <div id="loginpop" class="modal fade">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="row">
+          <div class="col-sm-6 login">
+          <h4>Login</h4>
+            <form class="" role="form" method="POST" action="{{ route('login') }}">
+              @csrf
+          <div class="form-group">
+            <label class="sr-only" for="exampleInputEmail2">Email address</label>
+            <input type="email" class="form-control @error('email') is-invalid @enderror" name="email" id="exampleInputEmail2" placeholder="Enter email" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+            @error('email')
+              <span class="invalid-feedback" role="alert">
+                  <strong>{{ $message }}</strong>
+              </span>
+            @enderror
+          </div>
+          <div class="form-group">
+            <label class="sr-only" for="exampleInputPassword2">Password</label>
+            <input id="exampleInputPassword2" type="password" class="form-control @error('password') is-invalid @enderror" name="password" placeholder="Password" required autocomplete="current-password">
+            @error('password')
+              <span class="invalid-feedback" role="alert">
+                  <strong>{{ $message }}</strong>
+              </span>
+            @enderror
+          </div>
+          <div class="checkbox">
+            <label>
+              <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                
+              <label class="form-check-label" for="remember">
+                  {{ __('Remember Me') }}
+              </label>
+            </label>
+          </div>
+          <button type="submit" class="btn btn-success">Sign in</button>
+        </form>          
+          </div>
+          <div class="col-sm-6">
+            <h4>New User Sign Up</h4>
+            <p>Join today and get updated with all the properties deal happening around.</p>
+            <button type="submit" class="btn btn-info"  onclick="window.location.href='register.php'">Join Now</button>
+          </div>
+  
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- /.modal -->
+  
+  
+  
+  </body>
+  </html>
+  
+  
+  
+  
