4f608db0aee4a5896a9a47f5a39f02044572fa50
Sebastian-Velazquez <sergiovelazquez1693@gmail.com>
2023-10-28 19:49:00 -0300

    terminado el estilo y creando el diagrama del DB

--- resources/views/index/contact.blade.php
+++ resources/views/index/contact.blade.php
@@ -28,7 +28,7 @@
                 
         </div>
   <div class="col-lg-6 col-sm-6 ">
-  <div class="well"><iframe width="100%" height="300" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=Pulchowk,+Patan,+Central+Region,+Nepal&amp;aq=0&amp;oq=pulch&amp;sll=37.0625,-95.677068&amp;sspn=39.371738,86.572266&amp;ie=UTF8&amp;hq=&amp;hnear=Pulchowk,+Patan+Dhoka,+Patan,+Bagmati,+Central+Region,+Nepal&amp;ll=27.678236,85.316853&amp;spn=0.001347,0.002642&amp;t=m&amp;z=14&amp;output=embed"></iframe></div>
+  <div class="well"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4413.913535448567!2d-60.62361815282184!3d-33.026950063470615!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95b7a9a133432f1b%3A0xf0ed0588a8ac0e04!2sInmobiliaria%20Flores!5e0!3m2!1ses-419!2sar!4v1698526118756!5m2!1ses-419!2sar" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></div>
   </div>
 </div>
 </div>
--- resources/views/index/productDetail.blade.php
+++ resources/views/index/productDetail.blade.php
@@ -18,31 +18,31 @@
 <div class="col-lg-3 col-sm-4 hidden-xs">
 
 <div class="hot-properties hidden-xs">
-<h4>Hot Properties</h4>
+<h4>Propiedades similares</h4>
 <div class="row">
                 <div class="col-lg-4 col-sm-5"><img src="images/properties/4.jpg" class="img-responsive img-circle" alt="properties"/></div>
                 <div class="col-lg-8 col-sm-7">
-                  <h5><a href="property-detail.php">Integer sed porta quam</a></h5>
+                  <h5><a href="{{ route('productDetail')}}">Casa 1</a></h5>
                   <p class="price">$300,000</p> </div>
               </div>
 <div class="row">
                 <div class="col-lg-4 col-sm-5"><img src="images/properties/1.jpg" class="img-responsive img-circle" alt="properties"/></div>
                 <div class="col-lg-8 col-sm-7">
-                  <h5><a href="property-detail.php">Integer sed porta quam</a></h5>
+                  <h5><a href="{{ route('productDetail')}}">Casa 2</a></h5>
                   <p class="price">$300,000</p> </div>
               </div>
 
 <div class="row">
                 <div class="col-lg-4 col-sm-5"><img src="images/properties/3.jpg" class="img-responsive img-circle" alt="properties"/></div>
                 <div class="col-lg-8 col-sm-7">
-                  <h5><a href="property-detail.php">Integer sed porta quam</a></h5>
+                  <h5><a href="{{ route('productDetail')}}">Casa 3</a></h5>
                   <p class="price">$300,000</p> </div>
               </div>
 
 <div class="row">
                 <div class="col-lg-4 col-sm-5"><img src="images/properties/2.jpg" class="img-responsive img-circle" alt="properties"/></div>
                 <div class="col-lg-8 col-sm-7">
-                  <h5><a href="property-detail.php">Integer sed porta quam</a></h5>
+                  <h5><a href="{{ route('productDetail')}}">Casa 4</a></h5>
                   <p class="price">$300,000</p> </div>
               </div>
 
@@ -60,7 +60,7 @@
 
 <div class="col-lg-9 col-sm-8 ">
 
-<h2>2 room and 1 kitchen apartment</h2>
+<h2>Apartamento de 2 habitaciones y 1 cocina.</h2>
 <div class="row">
   <div class="col-lg-8">
   <div class="property-images">
@@ -110,13 +110,13 @@
 
 
 
-  <div class="spacer"><h4><span class="glyphicon glyphicon-th-list"></span> Properties Detail</h4>
-  <p>Efficiently unleash cross-media information without cross-media value. Quickly maximize timely deliverables for real-time schemas. Dramatically maintain clicks-and-mortar solutions without functional solutions.</p>
-  <p>Completely synergize resource sucking relationships via premier niche markets. Professionally cultivate one-to-one customer service with robust ideas. Dynamically innovate resource-leveling customer service for state of the art customer service</p>
+  <div class="spacer"><h4><span class="glyphicon glyphicon-th-list"></span> Detalle de propiedades</h4>
+  <p>Descripcón</p>
+  <p>Descripcón</p>
 
   </div>
-  <div><h4><span class="glyphicon glyphicon-map-marker"></span> Location</h4>
-<div class="well"><iframe width="100%" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=Pulchowk,+Patan,+Central+Region,+Nepal&amp;aq=0&amp;oq=pulch&amp;sll=37.0625,-95.677068&amp;sspn=39.371738,86.572266&amp;ie=UTF8&amp;hq=&amp;hnear=Pulchowk,+Patan+Dhoka,+Patan,+Bagmati,+Central+Region,+Nepal&amp;ll=27.678236,85.316853&amp;spn=0.001347,0.002642&amp;t=m&amp;z=14&amp;output=embed"></iframe></div>
+  <div><h4><span class="glyphicon glyphicon-map-marker"></span> Ubicación</h4>
+<div class="well"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4413.913535448567!2d-60.62361815282184!3d-33.026950063470615!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95b7a9a133432f1b%3A0xf0ed0588a8ac0e04!2sInmobiliaria%20Flores!5e0!3m2!1ses-419!2sar!4v1698526118756!5m2!1ses-419!2sar" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe></div>
   </div>
 
   </div>
@@ -124,7 +124,7 @@
   <div class="col-lg-12  col-sm-6">
 <div class="property-info">
 <p class="price">$ 200,000,000</p>
-  <p class="area"><span class="glyphicon glyphicon-map-marker"></span> 344 Villa, Syndey, Australia</p>
+  <p class="area"><span class="glyphicon glyphicon-map-marker"></span> Juan B. Justo 2276, Villa Gdor. Galvez, Santa Fe</p>
   
   <div class="profile">
   <span class="glyphicon glyphicon-user"></span> Agent Details
